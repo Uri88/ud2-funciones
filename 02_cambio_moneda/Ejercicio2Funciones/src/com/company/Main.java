748c2811f123b7b23656b0337ee10fb6dfbfbf3f
Unknown <urisixx@gmail.com>
2017-11-06 20:42:44 +0100

    Ejercicio2Funciones

---
+++ 02_cambio_moneda/Ejercicio2Funciones/src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static double calcularCambio(double a, double cambio, String opcion) {
+
+        switch (opcion) {
+
+            case "usd":
+                cambio = a * 1.16;
+                break;
+            case "gbp":
+                cambio = a * 0.88;
+                break;
+            case "cny":
+                cambio = a * 7.7;
+                break;
+            case "jpy":
+                cambio = a * 132.54;
+                break;
+        }
+
+
+        return cambio;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // write your code here
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Introduce una cantidad en Euros: ");
+        double cantidad = Double.parseDouble(br.readLine());
+
+        System.out.println("Moneda a la que convertir (USD, GBP, CNY, JPY): ");
+        String convertir = br.readLine().toLowerCase();
+
+        double resultado = 0;
+
+        switch (convertir) {
+
+            case "usd":
+                System.out.println("--- " + cantidad + " euros son " + calcularCambio(cantidad, resultado, convertir) + " dolares ---");
+                break;
+            case "gbp":
+                System.out.println("--- " + cantidad + " euros son " + calcularCambio(cantidad, resultado, convertir) + " libras ---");
+                break;
+            case "cny":
+                System.out.println("--- " + cantidad + " euros son " + calcularCambio(cantidad, resultado, convertir) + " yuanes ---");
+                break;
+            case "jpy":
+                System.out.println("--- " + cantidad + " euros son " + calcularCambio(cantidad, resultado, convertir) + " yenes ---");
+                break;
+
+        }
+
+    }
+}
