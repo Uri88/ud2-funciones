a0cef50dd412d5167bad571112d0ea2b05c73aff
Unknown <urisixx@gmail.com>
2017-11-06 20:27:51 +0100

    Ejercicio1 Funciones

---
+++ 01_perimetro_rectangulo/Ejercicio1Funciones/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static int perimetroRectangulo(int altura, int base){
+
+        int area = (base*altura)/2;
+        return area;
+    }
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Introduce una base: ");
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.print("Introduce una altura: ");
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(perimetroRectangulo(a, b));
+
+    }
+}
