67eac8c749457f64d94d1d8162343a7cb70a885e
Unknown <urisixx@gmail.com>
2017-11-13 20:58:14 +0100

    Ejercicio8Funciones

---
+++ 08_factorial/Ejercicio8Funciones/src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static int factorepe(int a, int resultado) {
+
+        for (int i = 1; i <= a; i++) {
+
+            resultado = resultado * i;
+        }
+        return resultado;
+    }
+
+    public static int factorecu(int a) {
+        if (a == 0)
+            return 1;
+        else
+            return a * (factorecu(a - 1));
+    }
+
+    public static void main(String[] args) throws IOException {
+        // write your code here
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int resultado = 1;
+
+
+        System.out.print("Introduce un valor: ");
+        int a = Integer.parseInt(br.readLine());
+
+
+        System.out.println("El factorial reiterativo de " + 'a' + " es: " + (factorepe(a, resultado)));
+
+        System.out.println("El factorial recursivo de " + 'a' + " es: " + (factorecu(a)));
+
+
+    }
+}
